e0f03e7307050036a63f6c613d7f8f04cf09d3cf
adelgeiszt <cr0ssfit_1>
2021-01-29 16:23:28 +0100

    Spring webapp demo done

---
+++ src/main/java/guru/springframework/spring5webapp/controller/AuthorController.java
@@ -0,0 +1,22 @@
+package guru.springframework.spring5webapp.controller;
+
+import guru.springframework.spring5webapp.repos.AuthorRepo;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class AuthorController {
+    private final AuthorRepo authorRepo;
+
+    public AuthorController(AuthorRepo authorRepo) {
+        this.authorRepo = authorRepo;
+    }
+
+    @RequestMapping("/authors")
+    public String getAuthors(Model model) {
+        model.addAttribute("authors", authorRepo.findAll());
+        return "authorlist";
+    }
+
+}
